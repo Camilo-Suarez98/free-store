78a19c44ed458abf65b18426edcde592c244fd38
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-10-31 10:59:29 -0500

    Include a burger menu to mobile view

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,50 @@
+import { useState } from "react"
+
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false)
+
+  const handleShowMenu = () => {
+    if (!showMenu) setShowMenu(true)
+    else setShowMenu(false)
+  }
+
   return (
-    <div className="w-full absolute top-0 left-0 right-0">
-      <section className="w-24 p-4">
-        <img src="../../logo.png" alt="logo" />
+    <div className="w-full absolute top-0 left-0 right-0 flex justify-between items-center pt-4 px-4 sm:px-24 lg:px-4">
+      <section className="w-24">
+        <a href="/">
+          <img src="../../logo.png" alt="logo" />
+        </a>
+      </section>
+      <section className="sm:hidden">
+        <input type="checkbox" id="checkbox" onClick={handleShowMenu} />
+        <label htmlFor="checkbox" className="toggle">
+          <div className="bar bar--top"></div>
+          <div className="bar bar--middle"></div>
+          <div className="bar bar--bottom"></div>
+        </label>
+
+      </section>
+      {showMenu &&
+        <section className="absolute top-24 left-0 right-0 sm:hidden">
+          <ul className="flex flex-col justify-around">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/characters">Characters</a>
+            </li>
+          </ul>
+        </section>
+      }
+      <section className="hidden sm:block sm:w-60">
+        <ul className="flex justify-around">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/characters">Characters</a>
+          </li>
+        </ul>
       </section>
     </div>
   )
