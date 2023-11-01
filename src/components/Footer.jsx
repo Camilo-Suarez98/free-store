c838171de317ad521d73964ac6ae42564649646d
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-11-01 14:46:32 -0500

    Style footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,21 @@
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="mt-16 w-full text-center">
+      <h2>
+        Developed by
+        {' '}
+        <a
+          href="https://github.com/Camilo-Suarez98"
+          className="text-[#2a5fff]"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Camilo-Suarez98
+        </a>
+      </h2>
+      <p>© 2023</p>
+    </div>
   )
 }
 
