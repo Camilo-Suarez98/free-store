e1ea3c959788a8fa93883757eb3635f69511ba1f
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-11-01 11:58:17 -0500

    Complete info about home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,28 @@
 const Home = () => {
   return (
-    <div>
-      este es el home
+    <div className="text-center p-0">
+      <h2 className="text-4xl text-[#2a5fff]">Explore Rick and Morty&apos;s Universe</h2>
+      <h3 className="text-2xl mt-4">This app is a esential tool to all fans of this great serie, you can show a great content about Rick and Morty, including:</h3>
+      <ul className="my-6">
+        <li>
+          <p className="text-xl my-3">
+            <strong className="text-[#2a5fff]">Characters: </strong>
+            A complete gallery of characters from the serie. You can get a detailed information
+          </p>
+        </li>
+        <li>
+          <p className="text-xl my-3">
+            <strong className="text-[#2a5fff]">Episodes: </strong>
+            Find episode info like, season, chapter, and air date
+          </p>
+        </li>
+        <li>
+          <p className="text-xl my-3">
+            <strong className="text-[#2a5fff]">Locations: </strong>
+            Discover each fascinating place visited by Rick and Morty in all their adventures and information about each place
+          </p>
+        </li>
+      </ul>
     </div>
   )
 }
