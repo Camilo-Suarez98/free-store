436a13f504c473be08b28e4c6adf98d2fa0e7d72
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-11-01 18:51:07 -0500

    include loading and error methods in characters page

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -5,9 +5,11 @@ const Characters = () => {
     query {
       characters {
         results {
+          id
           name
           status
           image
+          gender
         }
       }
     }
@@ -16,8 +18,23 @@ const Characters = () => {
   const { loading, error, data } = useQuery(GET_CHARACTERS)
   console.log(data);
 
+  if (loading) {
+    return <div>loading...</div>
+  }
+
+  if (error) return <p>Error : {error.message}</p>;
+
   return (
-    <div>Characters</div>
+    <div>
+      {data.characters.results.map(({ id, name, status, image, gender }) => {
+        return (
+          <div key={id}>
+            <h2>{name}</h2>
+            <h3 className={status === 'Alive' ? 'text-green-500' : status === 'unknown' ? 'text-gray-500' : 'text-red-500'}>{status}</h3>
+          </div>
+        )
+      })}
+    </div>
   )
 }
 
