0c5fb77eb76870c2d8dc2ecfb54d4ccc8e8420f2
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-11-02 11:26:35 -0500

    Start to sho fetch request

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -26,14 +26,27 @@ const Characters = () => {
 
   return (
     <div>
-      {data.characters.results.map(({ id, name, status, image, gender }) => {
-        return (
-          <div key={id}>
-            <h2>{name}</h2>
-            <h3 className={status === 'Alive' ? 'text-green-500' : status === 'unknown' ? 'text-gray-500' : 'text-red-500'}>{status}</h3>
-          </div>
-        )
-      })}
+      <h2 className='text-xl'>Click on your favorite character to view details</h2>
+      <div className='flex flex-wrap justify-center gap-10 mt-10'>
+        {data.characters.results.map(({ id, name, status, image, gender }) => {
+          return (
+            <a
+              href={`/characters/${id}`}
+              key={id}
+              className='border-2 border-[#2a52c9] text-[#2a5fff] cursor-pointer p-4 rounded-lg transition duration-300 hover:bg-[#2a5fff] hover:text-white hover:text-bold'
+            >
+              <h2 className='text-3xl my-2 overflow-hidden text-ellipsis whitespace-nowrap'>{name}</h2>
+              <h3 className={status === 'Alive' ? 'text-green-500 text-2xl' : status === 'unknown' ? 'text-gray-500 text-2xl' : 'text-red-500 text-2xl'}>{status}</h3>
+              <p className='text-2xl my-2'>{gender}</p>
+              <img
+                src={image}
+                alt={name}
+                className='rounded-lg'
+              />
+            </a>
+          )
+        })}
+      </div>
     </div>
   )
 }
