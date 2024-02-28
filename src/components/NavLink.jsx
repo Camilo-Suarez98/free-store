1f4ad3b935ce2e66f912c4c3602eea54aa18a6de
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-02-28 10:31:38 -0500

    feat: change text color

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -3,7 +3,7 @@ const NavLink = ({ href, value }) => {
     <li>
       <a
         href={href}
-        className="flex items-center justify-center text-[#2a5fff] h-16 p-2 text-xl"
+        className="flex items-center justify-center text-[#53bf23] h-16 p-2 text-xl transition duration-500 hover:text-[#abfa88]"
       >
         {value}
       </a>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,23 @@
+import Paragraph from '../components/Paragraph';
+
 const Home = () => {
   return (
     <div className="text-center p-0">
-      <h2 className="text-3xl text-[#2a5fff] sm:text-4xl md:text-5xl min-[991px]:text-6xl">Explore Rick and Morty&apos;s Universe</h2>
+      <h2 className="text-3xl text-[#53bf23] sm:text-4xl md:text-5xl min-[991px]:text-6xl">Explore Rick and Morty&apos;s Universe</h2>
       <h3 className="text-2xl mt-4 sm:mt-8 md:text-4xl">This app is a esential tool to all fans of this great serie, you can show a great content about Rick and Morty, including:</h3>
       <ul className="my-6 sm:my-10">
-        <li>
-          <p className="text-xl my-3 md:text-3xl min-[991px]:text-4xl">
-            <strong className="text-[#2a5fff]">Characters: </strong>
-            A complete gallery of characters from the serie. You can get a detailed information
-          </p>
-        </li>
-        <li>
-          <p className="text-xl my-3 md:text-3xl min-[991px]:text-4xl">
-            <strong className="text-[#2a5fff]">Episodes: </strong>
-            Find episode info like, season, chapter, and air date
-          </p>
-        </li>
-        <li>
-          <p className="text-xl my-3 md:text-3xl min-[991px]:text-4xl">
-            <strong className="text-[#2a5fff]">Locations: </strong>
-            Discover each fascinating place visited by Rick and Morty in all their adventures and information about each place
-          </p>
-        </li>
+        <Paragraph
+          title="Characters:"
+          text="A complete gallery of characters from the serie. You can get a detailed information"
+        />
+        <Paragraph
+          title="Episodes:"
+          text="Find episode info like, season, chapter, and air date"
+        />
+        <Paragraph
+          title="Locations:"
+          text="Discover each fascinating place visited by Rick and Morty in all their adventures and information about each place"
+        />
       </ul>
     </div>
   );
