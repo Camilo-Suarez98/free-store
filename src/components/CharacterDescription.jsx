a7d31aee56297140de7ccf40af4a826d9f1cca0a
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-02-28 15:53:35 -0500

    feat: create new component to information about details of characters

---
+++ src/components/CharacterDescription.jsx
@@ -0,0 +1,9 @@
+const CharacterDescription = ({ title, value }) => {
+  return (
+    <p className="text-2xl">
+      <span className="text-[#53bf23] my-4">{title}: </span> {value}
+    </p>
+  );
+};
+
+export default CharacterDescription;
