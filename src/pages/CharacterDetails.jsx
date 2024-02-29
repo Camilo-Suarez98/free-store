d9f462eb2d4a77ffc351369aae2e41bdc111ad31
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-02-29 17:02:54 -0500

    feat: include information about episodes for any character

--- src/pages/CharacterDetails.jsx
+++ src/pages/CharacterDetails.jsx
@@ -1,6 +1,7 @@
 import { useQuery, gql } from "@apollo/client";
 import { useParams } from "react-router-dom";
 import Loader from "../components/Loader";
+import CharacterDescription from '../components/CharacterDescription';
 
 const GET_CHARACTER = gql`
   query getCharater($id: ID!) {
@@ -15,6 +16,12 @@ const GET_CHARACTER = gql`
       }
       location {
         name
+      }
+      episode {
+        id
+        name
+        air_date
+        episode
       }
     }
   }
@@ -32,13 +39,37 @@ const CharacterDetails = () => {
   if (loading) return <Loader />;
 
   if (error) return <p>Error : {error.message}</p>;
+  console.log({ data });
 
   return (
     <div>
       {
         data && (
-          <div>
-            <h2>{data.character.name}</h2>
+          <div
+            className="flex flex-col items-center"
+          >
+            <h2
+              className="text-3xl text-[#53bf23] my-6 sm:text-4xl md:text-5xl min-[991px]:text-6xl"
+            >
+              {data.character.name}
+            </h2>
+            <img
+              className="rounded-3xl my-5"
+              src={data.character.image}
+              alt={data.character.name}
+            />
+
+            <CharacterDescription title="Specie" value={data.character.species} />
+            <CharacterDescription title="Status" value={data.character.status} />
+            <CharacterDescription title="Origin" value={data.character.origin.name} />
+            <CharacterDescription title="Location" value={data.character.location.name} />
+
+            <h3 className="text-3xl my-6 text-[#53bf23]">Episodes and air date</h3>
+            <ol className="list-decimal">
+              {data.character.episode.map(({ id, name, air_date, episode }) => (
+                <li key={id}>{episode}: {name} - {air_date}</li>
+              ))}
+            </ol>
           </div>
         )
       }
