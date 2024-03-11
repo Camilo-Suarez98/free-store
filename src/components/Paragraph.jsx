c3f9663938a3a4e44096cc945b6600cb0af76652
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-03-11 11:34:49 -0500

    fix: change some paragraph about information

--- src/components/Paragraph.jsx
+++ src/components/Paragraph.jsx
@@ -2,7 +2,7 @@ const Paragraph = ({ title, text }) => {
   return (
     <li>
       <p className="text-xl my-3 md:text-3xl min-[991px]:text-4xl">
-        <strong className="text-[#53bf23]">{title} {" "}</strong>
+        <strong className="text-[#53bf23]">{title}: {" "}</strong>
         {text}
       </p>
     </li>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,20 +3,20 @@ import Paragraph from '../components/Paragraph';
 const Home = () => {
   return (
     <div className="text-center p-0">
-      <h2 className="text-3xl text-[#53bf23] sm:text-4xl md:text-5xl min-[991px]:text-6xl">Explore Rick and Morty&apos;s Universe</h2>
-      <h3 className="text-2xl mt-4 sm:mt-8 md:text-4xl">This app is a esential tool to all fans of this great serie, you can show a great content about Rick and Morty, including:</h3>
+      <h2 className="text-3xl text-[#53bf23] sm:text-4xl md:text-5xl min-[991px]:text-6xl">Explore the Rick and Morty Universe</h2>
+      <h3 className="text-2xl mt-4 sm:mt-8 md:text-4xl">This app is an esential tool to all fans of this great series, it can display great content about Rick and Morty, including:</h3>
       <ul className="my-6 sm:my-10">
         <Paragraph
-          title="Characters:"
-          text="A complete gallery of characters from the serie. You can get a detailed information"
+          title="Characters"
+          text="A complete gallery of characters from the series. You can get a detailed information"
         />
         <Paragraph
-          title="Episodes:"
-          text="Find episode info like, season, chapter, and air date"
+          title="Episodes"
+          text="Find information on episodes such as, season, episode and air date"
         />
         <Paragraph
-          title="Locations:"
-          text="Discover each fascinating place visited by Rick and Morty in all their adventures and information about each place"
+          title="Locations"
+          text="Discover every fascinating place visited by Rick and Morty in all their adventures and information about each location"
         />
       </ul>
     </div>
